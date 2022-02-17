59751eadf1f168f22cf8e5ffbb87bdb681079f9b
vifrac <vifrac@gmail.com>
2022-02-16 20:46:55 -0500

    test content croossExtension

--- testApi/background.js
+++ testApi/background.js
@@ -1,2 +1,29 @@
 var manifest = browser.runtime.getManifest();
-console.log('desde background : ' + manifest.name);
+console.log("desde background : " + manifest.name);
+
+function logTabs(tabs) {
+  console.log(tabs);
+}
+
+browser.tabs.query({ currentWindow: true }, logTabs);
+
+let querying = browser.tabs.query({});
+console.log(querying);
+let querying1 = browser.tabs.query({ active: true });
+console.log(querying1);
+
+function logCookie(c) {
+  console.log(c);
+}
+
+function logError(e) {
+  console.error(e);
+}
+
+let setCookie = browser.cookies.set({
+  url: "https://vifrac.com/",
+  name: "testCookies",
+});
+setCookie.then(logCookie, logError);
+
+console.log(setCookie);
